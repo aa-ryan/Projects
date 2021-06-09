0b968550ed97e95c9853949fc472acd69e0aaf47
aa-ryan <aryansinha6363@gmail.com>
2021-06-09 15:50:30 +0530

    reverted back to original structures

--- Rust/toychain/src/chain.rs
+++ Rust/toychain/src/chain.rs
@@ -6,21 +6,66 @@ extern crate sha2;
 use self::sha2::{Sha256, Digest};
 use std::fmt::Write;
 
-mod structures;
+#[derive(Debug, Clone, Serialize)]
+struct Transaction {
+    sender: String,
+    receiver: String,  
+    amount: f32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Blockheader {
+    timestamp: i64,
+    nonce: u32, 
+    pre_hash: String,  
+    merkle: String,  
+    difficulty: u32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Block {
+    header: Blockheader,
+    count: u32,
+    transactions: Vec<Transaction>
+}
+
+pub struct Chain {
+    chain: Vec<Block>,
+    curr_trans: Vec<Transaction>,
+    difficulty: u32,
+    miner_addr: String, 
+    reward: f32,
+}
 
 impl Chain {
+    pub fn new(miner_addr: String, difficulty: u32) -> Chain {
+    let mut chain = Chain {
+        chain: Vec::new(),
+        curr_trans: Vec::new(),
+        difficulty,
+        miner_addr,
+        reward: 100.0,
+    };
 
-    pub fn new(minerAddress: String, difficulty: u32) -> Chain {
-        let mut chain = Chain {
-            chain: Vec::new(),
-            currentTransaction: Vec::new(),
-            difficulty,
-            minerAddress,
-            reward: 100.0,
-        };
-
-        chain.generate_new_block();
-        chain
+    chain.generate_new_block();
+    chain
     }
 
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
+        self.curr_trans.push(Transaction{
+            sender, 
+            receiver,
+            amount,
+        });
+        true
+    }
+
+    pub fn last_hash(&self) -> String {
+        let block = match self.chain.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48; 64]).unwrap()
+        };
+
+        Chain::hash(&block.header)
+    }
 }
