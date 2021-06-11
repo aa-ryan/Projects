720bbdb4ddbdd96ef27ee670638e10a007671da2
aa-ryan <aryansinha6363@gmail.com>
2021-06-11 18:25:38 +0530

    Orientation fix

--- Rust/toychain/src/chain.rs
+++ Rust/toychain/src/chain.rs
@@ -39,46 +39,6 @@ pub struct Chain {
 }
 
 impl Chain {
-    pub fn new(miner_addr: String, difficulty: u32) -> Chain {
-    let mut chain = Chain {
-        chain: Vec::new(),
-        curr_trans: Vec::new(),
-        difficulty,
-        miner_addr,
-        reward: 100.0,
-    };
-
-    chain.generate_new_block();
-    chain
-    }
-
-    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
-        self.curr_trans.push(Transaction{
-            sender, 
-            receiver,
-            amount,
-        });
-        true
-    }
-
-    pub fn last_hash(&self) -> String {
-        let block = match self.chain.last() {
-            Some(block) => block,
-            None => return String::from_utf8(vec![48; 64]).unwrap()
-        };
-
-        Chain::hash(&block.header)
-    }
-
-    pub fn update_difficulty(&mut self, difficulty: u32) -> bool {
-        self.difficulty = difficulty;
-        true
-    }
-
-    pub fn update_reward(&mut self, reward: f32) -> bool {
-        self.reward = reward;
-        true
-    }
 
     pub fn generate_new_block(&mut self) -> bool {
         let header = Blockheader {
@@ -173,6 +133,47 @@ impl Chain {
         }
         s
     }
+    pub fn new(miner_addr: String, difficulty: u32) -> Chain {
+    let mut chain = Chain {
+        chain: Vec::new(),
+        curr_trans: Vec::new(),
+        difficulty,
+        miner_addr,
+        reward: 100.0,
+    };
+
+    chain.generate_new_block();
+    chain
+    }
+
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
+        self.curr_trans.push(Transaction{
+            sender, 
+            receiver,
+            amount,
+        });
+        true
+    }
+
+    pub fn last_hash(&self) -> String {
+        let block = match self.chain.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48; 64]).unwrap()
+        };
+
+        Chain::hash(&block.header)
+    }
+
+    pub fn update_difficulty(&mut self, difficulty: u32) -> bool {
+        self.difficulty = difficulty;
+        true
+    }
+
+    pub fn update_reward(&mut self, reward: f32) -> bool {
+        self.reward = reward;
+        true
+    }
+
 
 
 }
