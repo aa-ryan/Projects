930c3e3d69a6a2aebfce95a64ecc63be756f40e7
aa-ryan <aryansinha6363@gmail.com>
2021-06-11 13:03:34 +0530

    started main

--- Rust/toychain/src/main.rs
+++ Rust/toychain/src/main.rs
@@ -1,1 +1,28 @@
+#[macro_use]
+extern crate serde_derive;
+
+use std::io;
+use std::process;
+use std::io::Write;
+
 mod chain;
+
+fn main() {
+    let mut miner_addr = String::new();
+    let mut difficulty = String::new();
+    let mut choice = String::new();
+
+    print!("Input Miner Address: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut miner_addr);
+    print!("Difficulty: ");;
+    io::stdout().flush();
+    io::stdin().read_line(&mut difficulty);
+
+    let diff = difficulty.trim().parse::<u32>().expect("We need an integer");
+    println!("Generating Genesis Block!");
+    let mut chain = chain::Chain::new(miner_addr
+                                .trim()
+                                .to_string(), 
+                                diff);
+}
