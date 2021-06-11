c591f697de694c4f51291766ddfed0fb596ef79c
aa-ryan <aryansinha6363@gmail.com>
2021-06-11 18:19:10 +0530

    fixed sender and receiver

--- Rust/toychain/src/main.rs
+++ Rust/toychain/src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     loop {
         println!("Menu");
         println!("1) New Transaction\n2) Mine Block\n3) Change Difficulty\n4) Change Reward\n5)Exit");
-        println!("Enter your choice: ");
+        print!("Enter your choice: ");
         io::stdout().flush();
         choice.clear();
         io::stdin().read_line(&mut choice);
@@ -56,10 +56,10 @@ fn new_t(chain: &mut chain::Chain) {
     let mut sender = String::new();
     let mut receiver = String::new();
     let mut amount = String::new();
-    print!("Enter sender address: ");
+    print!("Enter receiver address: ");
     io::stdout().flush();
     io::stdin().read_line(&mut sender);
-    print!("Enter the receiver address: ");
+    print!("Enter the sender address: ");
     io::stdout().flush();
     io::stdin().read_line(&mut receiver);
     print!("Enter amount: ");
@@ -88,7 +88,7 @@ fn gen_b(chain: &mut chain::Chain) {
 
 fn diff_c(chain: &mut chain::Chain) {
     let mut new_diff = String::new();
-    print!("Enter new Difficulty");
+    print!("Enter new Difficulty: ");
     io::stdout().flush();
     io::stdin().read_line(&mut new_diff);
     let res = chain.update_difficulty(new_diff.trim().parse().unwrap());
