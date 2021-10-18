8c224fe707ed7b1a5174534f6729ea8324a0eb6f
aa-ryan <aryansinha6363@gmail.com>
2021-10-19 02:16:38 +0530

    capitalize client added

--- Java/Extras/JavaNet/Simple_Threaded_Server/CapitalizeClient.java
+++ Java/Extras/JavaNet/Simple_Threaded_Server/CapitalizeClient.java
@@ -1,5 +1,3 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,7 +10,15 @@ public class CapitalizeClient {
 		}
 		try (var socket = new Socket(args[0], 59898)) {
 			System.out.println("Enter lines of text then Ctrl+D to quit");
+			var scanner = new Scanner(System.in);
+			var in = new Scanner(socket.getInputStream());
+			var out = new PrintWriter(socket.getOutputStream(), true);
+			while (scanner.hasNextLine()) {
+				out.println(scanner.nextLine());
+				System.out.println(in.nextLine());
+			}
 
+			scanner.close();
 		}
 	}
 }
