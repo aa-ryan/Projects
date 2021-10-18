da483806bdb02401f3ab8fd0616ab4b5d0f606e2
aa-ryan <aryansinha6363@gmail.com>
2021-10-19 01:58:54 +0530

    capitalize client start

---
+++ Java/Extras/JavaNet/Simple_Threaded_Server/CapitalizeClient.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class CapitalizeClient {
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("Pass the server IP as the sole command line argument");
+			return;
+		}
+		try (var socket = new Socket(args[0], 59898)) {
+			System.out.println("Enter lines of text then Ctrl+D to quit");
+
+		}
+	}
+}
