c3fb671773a920555f17de3219fe92d8007a2c99
aa-ryan <aryansinha6363@gmail.com>
2021-10-19 01:58:35 +0530

    capitalize server running

--- Java/Extras/JavaNet/Simple_Threaded_Server/CapitalizeServer.java
+++ Java/Extras/JavaNet/Simple_Threaded_Server/CapitalizeServer.java
@@ -25,6 +25,7 @@ public class CapitalizeServer {
 		try (var listener = new ServerSocket(59898)) {
 			System.out.println("The capitalization server is running...");
 			var pool = Executors.newFixedThreadPool(20);
+			// In Java, never create threads directly; instead, employ a "thread pool" and use and executor service to manage the threads."
 			while (true) {
 				pool.execute(new Capitalizer(listener.accept()));
 			}
@@ -44,8 +45,16 @@ public class CapitalizeServer {
 				var in = new Scanner(socket.getInputStream());
 				var out = new PrintWriter(socket.getOutputStream(), true);
 				while (in.hasNextLine()) {
-					out.println(in.nextLine().toUpperCase())
+					out.println(in.nextLine().toUpperCase());
 				}
+			} catch (Exception e) {
+				System.out.println("Error: " + socket);
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+				System.out.println("Closed: " + socket);
 			}
 		}
 	}
